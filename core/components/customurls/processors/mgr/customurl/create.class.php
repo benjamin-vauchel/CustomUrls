dc8f26a0932a39a417527e473858d80ce470db15
Benjamin Vauchel <contact@omycode.fr>
2012-08-23 22:07:38 +0200

    Fix beforeSave method of CustomUrlCreateProcessor

--- core/components/customurls/processors/mgr/customurl/create.class.php
+++ core/components/customurls/processors/mgr/customurl/create.class.php
@@ -6,12 +6,11 @@ class CustomUrlCreateProcessor extends modObjectCreateProcessor {
  
     public function beforeSave() 
     {
-        $this->setProperty('usergroup',intval($this->getProperty('usergroup',0)));
+        $this->object->set('usergroup',intval($this->getProperty('usergroup',0)));
+        $this->object->set('uri', ($this->getProperty('uri',false) == 'true' ? true : false));
+        $this->object->set('override', ($this->getProperty('override',false) == 'true' ? true : false));
+        $this->object->set('active', ($this->getProperty('active',false) == 'true' ? true : false));
 
-        $this->setProperty('uri', ($this->getProperty('uri',false) == 'true' ? true : false));
-        $this->setProperty('override', ($this->getProperty('override',false) == 'true' ? true : false));
-        $this->setProperty('active', ($this->getProperty('active',false) == 'true' ? true : false));
-        
         return parent::beforeSave();
     }
 }
